e771776b460c0e6bb16a2e3019233d5298d45d0f
Edy Appenzeller <edy@agentstack.it.com>
2026-08-29 03:54:19 -0600

    chore: switch canonical domain to agentstack.ink

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://agentstack.it.com',
+  site: 'https://agentstack.ink',
   trailingSlash: 'never',
   build: {
     format: 'directory',
